30e098c8e57321649196ddac8688e1a286238fa4
OmirLife <omirkemelbekuly@gmail.com>
2025-06-07 15:52:20 +0500

    Fuch this shit

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from yt_dlp import YoutubeDL
 from aiohttp import web
 from datetime import datetime
 from dotenv import load_dotenv
+from aiogram.utils.executor import start_webhook
 
 load_dotenv()
 
@@ -126,5 +127,12 @@ def main():
     return app
 
 if __name__ == '__main__':
-    app = main()
-    web.run_app(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 3000))
+    )
